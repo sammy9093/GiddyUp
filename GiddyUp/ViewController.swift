eeb2ba450cc17942802cebc4f4a8d312e5fafcc7
Samuel Ortega <samy9093@gmail.com>
2023-01-17 02:29:07 -0600

    Initial Commit

---
+++ GiddyUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GiddyUp
+//
+//  Created by Sammy on 1/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
