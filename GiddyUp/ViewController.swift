393163eae4803cb1ca0d0fbb43c1b6c0bafdea91
Samuel Ortega <samy9093@gmail.com>
2023-01-17 02:58:05 -0600

    Added Show Message Button

--- GiddyUp/ViewController.swift
+++ GiddyUp/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("🫰 viewDidLoad has run!")
+        messageLabel.text = "So Fab!"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        print("😎 The message button was pressed!")
+        messageLabel.text = "You are Awesome!"
+    }
+    
 }
 
